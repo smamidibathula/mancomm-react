01e08dac923ecf2ef091ee646356d4795df47a94
smamidibathula <srimamidibathula@gmail.com>
2024-04-29 22:40:15 +0530

    add base in config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-beautiful-dnd/",
+  base: "https://github.com/smamidibathula/mancomm-react.git",
   plugins: [react()],
 })
